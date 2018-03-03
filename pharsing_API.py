e7415a475bf106df5f857a4ebfe84d2e68210c86
gLoom2 <dlxorb1012@gmail.com>
2018-03-03 22:59:03 +0900

    second commit

--- pharsing_API.py
+++ pharsing_API.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\\Users\\yoyo1\\Desktop\\melee\\develop\\chromedriver_win32\\chromedriver')
+driver = webdriver.Chrome('\\Users\\yoyo1\\Desktop\\melee\\develop\\chromedriver_win32\\chromedriver')
 driver.implicitly_wait(3)
 
 url = 'http://overwatch.inven.co.kr/overank/rank/all/season5/summary/all/page/1/'
@@ -41,7 +41,10 @@ def get_user_data(url):
 user_url_list = get_user_list(get_url_html_soup(url))
 user_data_list = []
 data_list = []
+max_page_list = [6466, ]
 for i in range(len(user_url_list)):
+    if(len(user_url_list[i]) < 2):
+        continue
     for k in range(len(user_url_list[i])):
         # index 0: 최고점수, 1: 총 게임 수, 2: 승률, 3: KDA
         for j in range(4):
@@ -51,9 +54,11 @@ for i in range(len(user_url_list)):
                 break
 
             data_list.append(data[j+1].find('span').get_text())
-
+    if data_list[len(data_list)-1] == '\n' or data_list[len(data_list)-5] == '\n':
+        continue
     data_list.append('\n')  # 플레이어간 구분
 
+print(data_list)
 index = 0
 for i in range((len(data_list) - 49) // 4):  # 수정필요 3/2
     if data_list[index] == '\n':
